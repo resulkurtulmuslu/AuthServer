166ad40bdd7ebef56eb455c37ce82dd335c100a6
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-03 20:51:11 +0300

    core layer entity

---
+++ Core.AuthServer/Core.AuthServer.CoreLayer/Models/UserRefleshToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AuthServer.CoreLayer.Models
+{
+    public class UserRefleshToken
+    {
+        public string UserId { get; set; }
+
+        public string Code { get; set; }
+
+        public DateTime Expration { get; set; }
+    }
+}
