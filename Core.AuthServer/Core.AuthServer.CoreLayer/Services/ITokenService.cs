075ed6b890796bf2574a1f9db7eeea2fc4fec745
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-03 22:02:32 +0300

    ITokenService

---
+++ Core.AuthServer/Core.AuthServer.CoreLayer/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using Core.AuthServer.CoreLayer.Configuration;
+using Core.AuthServer.CoreLayer.DTOs;
+using Core.AuthServer.CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AuthServer.CoreLayer.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
