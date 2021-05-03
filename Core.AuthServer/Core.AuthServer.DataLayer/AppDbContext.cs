ee571bde6199157455c692cce983dc7c7eb90dfd
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-03 22:42:14 +0300

    AppDbContext

---
+++ Core.AuthServer/Core.AuthServer.DataLayer/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.AuthServer.CoreLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.AuthServer.DataLayer
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
