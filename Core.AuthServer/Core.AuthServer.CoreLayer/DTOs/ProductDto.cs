88a1ac7936eef7419f11cb0013d910f6472dc1b5
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-03 23:42:39 +0300

    AutoMapper

---
+++ Core.AuthServer/Core.AuthServer.CoreLayer/DTOs/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AuthServer.CoreLayer.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public Decimal TotalPrice => Price * Stock;
+
+        public string UserId { get; set; }
+    }
+}
---
+++ Core.AuthServer/Core.AuthServer.ServiceLayer/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Core.AuthServer.CoreLayer.DTOs;
+using Core.AuthServer.CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AuthServer.ServiceLayer
+{
+    class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
