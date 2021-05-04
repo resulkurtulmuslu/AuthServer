70ade0e2b832191864eeb5d47818f3f58a3829cb
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-04 20:35:08 +0300

    SignService

--- Core.AuthServer/Core.AuthServer.ServiceLayer/Services/ServiceGeneric.cs
+++ Core.AuthServer/Core.AuthServer.ServiceLayer/Services/ServiceGeneric.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Dtos;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.AuthServer.ServiceLayer.Services
