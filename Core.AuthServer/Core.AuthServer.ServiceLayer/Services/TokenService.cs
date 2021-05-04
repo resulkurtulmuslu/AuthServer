2727b50e0c3c957e4a95b23bf6bff090a167444e
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-05-04 21:24:40 +0300

    TokenService

--- Core.AuthServer/Core.AuthServer.ServiceLayer/Services/TokenService.cs
+++ Core.AuthServer/Core.AuthServer.ServiceLayer/Services/TokenService.cs
@@ -98,7 +98,30 @@ namespace Core.AuthServer.ServiceLayer.Services
 
         public ClientTokenDto CreateTokenByClient(Client client)
         {
-            throw new NotImplementedException();
+            var accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+            var securityKey = SignService.GetSymmetricSecurityKey(_tokenOptions.SecurityKey);
+
+            SigningCredentials signingCredentials =
+                new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                issuer: _tokenOptions.Issuer,
+                expires: accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims: GetClaimsByClient(client),
+                signingCredentials: signingCredentials);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwtSecurityToken);
+
+            var clientTokenDto = new ClientTokenDto
+            {
+                AccessToken = token,
+                AccessTokenExpiration = accessTokenExpiration
+            };
+
+            return clientTokenDto;
         }
     }
 }
